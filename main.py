ec429b9b97d5b7b39912bbacf384d7295ec18e40
Yosua Vega <yosuavega3gp@gmail.com>
2022-03-13 03:03:17 +0700

    create setting class

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pygame
 import sys
+from settings import *
 
 pygame.init()
-screen_width = 1200
-screen_height = 700
+
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
---
+++ settings.py
@@ -0,0 +1,19 @@
+level_map = [
+    '                            ',
+    '                            ',
+    '                            ',
+    ' XX    XXX            XX    ',
+    ' XX                         ',
+    ' XXXX         XX         XX ',
+    ' XXXX       XX              ',
+    ' XX    x  XXXX    XX  XX    ',
+    '       x  XXXX    XX  XXX   ',
+    '    XXXX  XXXXXX  XX  XXXX  ',
+    ' XXXXXXX  XXXXXX  XX  XXXX  ',
+]
+
+tile_size = 64
+screen_width = 1200
+screen_height = len(level_map) * tile_size
+
+print(len(level_map))
